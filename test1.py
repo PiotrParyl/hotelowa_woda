800da96e12a89632a4408b7e25298e7af92b456f
piotr <piotrekzrydek@gmial.com>
2022-10-26 20:32:24 +0200

    chuj123

--- test1.py
+++ test1.py
@@ -40,7 +40,7 @@ def water_per_day():
         #print(chuj[1], chuj[2])
 
         dzien7.append(chuj[1])
-        
+
 
     # Dzeien 6
 
@@ -103,13 +103,13 @@ def water_per_day():
         print(x)
      
 
-    _7_dni_temu = int(chuj_lista[288]) - int(chuj_lista[0])
-    _6_dni_temu = int(chuj_lista[577]) - int(chuj_lista[289])
-    _5_dni_temu = int(chuj_lista[866]) - int(chuj_lista[578])
-    _4_dni_temu = int(chuj_lista[1155]) - int(chuj_lista[867])
-    _3_dni_temu = int(chuj_lista[1444]) - int(chuj_lista[1156])
-    _2_dni_temu = int(chuj_lista[1733]) - int(chuj_lista[1445])
-    _1_dni_temu = int(chuj_lista[2022]) - int(chuj_lista[1734])
+    _7_dni_temu = int(dzien7[287]) - int(dzien7[0])
+    _6_dni_temu = int(dzien6[287]) - int(dzien6[0])
+    _5_dni_temu = int(dzien5[287]) - int(dzien5[0])
+    _4_dni_temu = int(dzien4[287]) - int(dzien4[0])
+    _3_dni_temu = int(dzien3[287]) - int(dzien3[0])
+    _2_dni_temu = int(dzien2[287]) - int(dzien2[0])
+    _1_dni_temu = int(dzien1[287]) - int(dzien1[0])
 
     print (f" 1 dni temu: {_1_dni_temu} dm3 \n 2 dni temu: {_2_dni_temu} \n 3 dni temu: {_3_dni_temu} \n 4 dni temu: {_4_dni_temu} \n 5 dni temu: {_5_dni_temu} \n 6 dni temu: {_6_dni_temu} \n 7 dni temu: {_7_dni_temu}")
 

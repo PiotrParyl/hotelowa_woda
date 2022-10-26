2440ce860c848f19f11a8a75c305f197863a43a3
piotr <piotrekzrydek@gmial.com>
2022-10-26 20:16:13 +0200

    chuj123

--- test1.py
+++ test1.py
@@ -36,13 +36,13 @@ def water_per_day():
 
     _7_dni_temu = int(chuj_lista[288]) - int(chuj_lista[0])
     _6_dni_temu = int(chuj_lista[577]) - int(chuj_lista[289])
-    _5_dni_temu = int(chuj_lista[865]) - int(chuj_lista[578])
-    _4_dni_temu = int(chuj_lista[1154]) - int(chuj_lista[866])
-    _3_dni_temu = int(chuj_lista[1443]) - int(chuj_lista[1155])
-    _2_dni_temu = int(chuj_lista[1732]) - int(chuj_lista[1444])
-    _1_dni_temu = int(chuj_lista[2021]) - int(chuj_lista[1732])
+    _5_dni_temu = int(chuj_lista[866]) - int(chuj_lista[578])
+    _4_dni_temu = int(chuj_lista[1155]) - int(chuj_lista[867])
+    _3_dni_temu = int(chuj_lista[1444]) - int(chuj_lista[1156])
+    _2_dni_temu = int(chuj_lista[1733]) - int(chuj_lista[1445])
+    _1_dni_temu = int(chuj_lista[2022]) - int(chuj_lista[1734])
 
-    print (f"Wczoraj zuzyto: {_1_dni_temu} dm3 \n 2 dni temu: {_2_dni_temu} \n 3 dni temu: {_3_dni_temu} \n 4 dni temu: {_4_dni_temu} \n 5 dni temu: {_5_dni_temu} \n 6 dni temu: {_6_dni_temu} \n 7 dni temu: {_7_dni_temu}")
+    print (f"1 dni temu: {_1_dni_temu} dm3 \n 2 dni temu: {_2_dni_temu} \n 3 dni temu: {_3_dni_temu} \n 4 dni temu: {_4_dni_temu} \n 5 dni temu: {_5_dni_temu} \n 6 dni temu: {_6_dni_temu} \n 7 dni temu: {_7_dni_temu}")
 
 
 print(water_per_day())
